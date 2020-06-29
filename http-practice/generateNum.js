55106120f4ccdacfbee91b6e449a7c61870f449d
Rethabile Segoe <precious.segoe@gmail.com>
2020-06-29 22:32:01 +0200

    add generate new id option

--- http-practice/generateNum.js
+++ http-practice/generateNum.js
@@ -1,9 +1,18 @@
-module.exports = function generateID() {
-  let characters = "abdegjkmnpqrvwxyz23456789"; //all lowercase, remove common profanities + 1/0
-  let IDlength = 6;
+let characters = "abdegjkmnpqrvwxyz23456789"; //all lowercase, remove common profanities + 1/0
+let IDlength = 6;
+module.exports = function generateID(data) {
+  let result = generateRandomId();
+  let checkDuplication = data.find((num) => num.id === result);
+  if (checkDuplication !== undefined) {
+    return generateID(data);
+  }
+  return result;
+};
+
+function generateRandomId() {
   let result = "";
   for (let i = 0; i < IDlength; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return result;
-};
+}
