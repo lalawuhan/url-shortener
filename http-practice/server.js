17febb59b6fbdce4ef0e7ef2741147207f9e0f5b
Rethabile Segoe <precious.segoe@gmail.com>
2020-07-02 17:28:14 +0200

    add html responses, and path to serve html file

--- http-practice/server.js
+++ http-practice/server.js
@@ -1,20 +1,23 @@
 const polka = require("polka");
+const bodyParser = require("body-parser");
+const path = require("path");
+
 const app = polka({
-  onError: (err, req, res) => {
-    const code = (res.statusCode = err.code || err.status || 500);
-    if (code === 500 && process.env.NODE_ENV === "production") {
-      res.end(http.STATUS_CODES[code]);
-      return;
-    }
-    res.end(err.length && (err || err.message || http.STATUS_CODES[code]));
+  onError(err, req, res, next) {
+    const http = require("http");
+    console.log(err);
+    let code = (res.statusCode = err.code || err.status || 500);
+    res.end((err.length && err) || err.message || http.STATUS_CODES[code]);
   },
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const port = 3000;
 const generateID = require("./generateNum.js");
 const fs = require("fs");
-
 const dataPath = "./data/links.json";
+const homePage = path.join(__dirname, "index.html");
 
 let data;
 try {
@@ -25,28 +28,43 @@ try {
     err.status = 404;
     throw err;
   } else {
-    const err = new Error();
+    const err = new Error("Error reading data");
     throw err;
   }
 }
 
 app.get("/", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  try {
-    res.statusCode = 200;
-    res.end("Homepage");
-  } catch (err) {
-    console.error(err);
-  }
+  fs.readFile(homePage, { encoding: "utf-8" }, function (err, data) {
+    if (!err) {
+      console.log("received data: " + data);
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    } else {
+      console.log(err);
+    }
+  });
 });
 
+function renderList(data) {
+  return `<body>
+  <ul>
+  ${data.map((el) => {
+    return `
+    <li> <strong>url:</strong> ${el.url}</li>
+    <li> <strong>id:</strong> ${el.id}</li>
+    `;
+  })}
+  </ul>
+  </body>
+  `;
+}
+
 app.get("/links", (req, res) => {
-  res.setHeader("Content-Type", "application/json");
-  try {
-    res.end(JSON.stringify(data));
-  } catch (err) {
-    console.error(err);
-  }
+  res.writeHead(200, {
+    "Content-Type": "text/html",
+  });
+  res.end(renderList(data));
 });
 
 app.get("/links/:id", (req, res) => {
@@ -71,27 +89,43 @@ app.get("/links/:id", (req, res) => {
 });
 
 app.post("/links", (req, res) => {
-  try {
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-    req.on("end", () => {
+  console.log("headers", req.headers);
+  console.log("content", req.headers["content-type"]);
+  if (req.headers["content-length"] === "0") {
+    res.statusCode = 400;
+    res.setHeader("Content-Type", "application/json");
+    res.end(
+      JSON.stringify({
+        message: "Data is empty",
+      })
+    );
+  }
+  if (req.headers["content-type"] === "application/json") {
+    res.end(
+      JSON.stringify({
+        status: res.statusCode,
+        message: `Link successfully added`,
+      })
+    );
+  } else {
+    try {
+      let body = req.body.longurl;
       let newLink = {
         id: generateID(data),
         url: JSON.parse(body),
       };
       data.push(newLink);
-      res.statusCode = 200;
-      res.end(
-        JSON.stringify({
-          status: res.statusCode,
-          message: `Link successfully added`,
-        })
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+      res.write(
+        `<p>${newLink.url} successfully added. Id is ${newLink.id}</p>`
       );
-    });
-  } catch (err) {
-    console.error(err);
+      res.write(renderList(data));
+      res.end();
+    } catch (err) {
+      console.error(err);
+    }
   }
 });
 
@@ -100,25 +134,14 @@ app.put("/links/:id", (req, res) => {
   const id = req.params.id;
   let link = data.find((link) => link.id === id);
   if (link) {
-    let body = "";
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-    req.on("end", () => {
-      let linkUpdate = JSON.parse(body);
-      let prevLink = link.url;
-      let newLink = linkUpdate.url;
-      link.url = newLink;
-      res.statusCode = 200;
-      res.end(
-        JSON.stringify({
-          status: res.statusCode,
-          message: `Link successfully updated`,
-          prevLink: prevLink,
-          newLink: newLink,
-        })
-      );
-    });
+    let body = req.body;
+    let newLink = body.url;
+    let prevLink = link.url;
+    link.url = newLink;
+    res.statusCode = 200;
+    res.write(`<p>${prevLink} successfully updated to ${newLink}.</p>`);
+    res.write(renderList(data));
+    res.end();
   } else {
     const err = new Error("Cannot update link");
     err.status = 404;
