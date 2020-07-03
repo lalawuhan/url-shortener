d1c6fedd53496e911ae28f2f252e8fdc655ea6d4
Rethabile Segoe <precious.segoe@gmail.com>
2020-07-03 14:04:11 +0200

    moved routes to seperate file

--- http-practice/server.js
+++ http-practice/server.js
@@ -1,24 +1,9 @@
 const polka = require("polka");
-const bodyParser = require("body-parser");
-const path = require("path");
-
-const app = polka({
-  onError(err, req, res, next) {
-    const http = require("http");
-    console.log(err);
-    let code = (res.statusCode = err.code || err.status || 500);
-    res.end((err.length && err) || err.message || http.STATUS_CODES[code]);
-  },
-});
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-const port = 3000;
-const generateID = require("./generateNum.js");
-const renderList = require("./templates/renderList");
 const fs = require("fs");
+const links = require("./routes/links");
+const bodyParser = require("body-parser");
+const port = 3000;
 const dataPath = "./data/links.json";
-const homePage = path.join(__dirname, "index.html");
 
 let data;
 try {
@@ -34,204 +19,18 @@ try {
   }
 }
 
-function accepts(req) {
-  if (req.headers.accept.indexOf("application/json") !== -1) {
-    return "json";
-  } else {
-    return "html";
-  }
-}
-
-app.get("/", (req, res) => {
-  fs.readFile(homePage, { encoding: "utf-8" }, function (err, data) {
-    if (!err) {
-      if (accepts(req) === "json") {
-        res.writeHead(200, {
-          "Content-Type": "application/json",
-        });
-        res.end(
-          JSON.stringify({
-            links: data,
-          })
-        );
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(data);
-        res.end();
-      }
-    } else {
-      console.log(err);
-    }
-  });
+const app = polka({
+  onError(err, req, res, next) {
+    const http = require("http");
+    console.log(err);
+    let code = (res.statusCode = err.code || err.status || 500);
+    res.end((err.length && err) || err.message || http.STATUS_CODES[code]);
+  },
 });
 
-app.get("/links", (req, res) => {
-  if (accepts(req) === "json") {
-    console.log("accepts json");
-    res.writeHead(200, {
-      "Content-Type": "application/json",
-    });
-    res.end(
-      JSON.stringify({
-        links: data,
-      })
-    );
-  } else {
-    res.writeHead(200, {
-      "Content-Type": "text/html",
-    });
-    res.end(renderList(data));
-  }
-});
-
-app.get("/links/:id", (req, res) => {
-  const id = req.params.id;
-  let link = data.find((link) => link.id === id);
-  if (link) {
-    if (accepts(req) === "json") {
-      res.setHeader("Content-Type", "application/json");
-      res.writeHead(301, {
-        Location: `${link.url}`,
-      });
-      res.end();
-    } else {
-      res.writeHead(301, {
-        Location: `${link.url}`,
-      });
-      res.end();
-    }
-  } else {
-    res.statusCode = 404;
-    res.end(
-      JSON.stringify({
-        status: 404,
-        message: "Link not found",
-      })
-    );
-  }
-});
-//TODO: accept headers, maybe add accept middleware, app.ise req.accept =  accepts(req)
-
-//TODO: moved template strings
-app.post("/links", (req, res) => {
-  console.log("headers", req.headers);
-  console.log("content", req.headers["content-type"]);
-  if (req.headers["content-length"] === "0") {
-    res.writeHead(400, {
-      "Content-Type": "application/json",
-    });
-    res.end(
-      JSON.stringify({
-        message: "Data is empty",
-      })
-    );
-  } else {
-    let body = req.body.url;
-    console.log("body", body);
-
-    let newLink = {
-      id: generateID(data),
-      url: body,
-    };
-    data.push(newLink);
-    if (accepts(req) === "json") {
-      res.writeHead(200, {
-        "Content-Type": "application/json",
-      });
-      res.end(
-        JSON.stringify({
-          status: res.statusCode,
-          message: `Link successfully added`,
-          links: data,
-        })
-      );
-    } else {
-      res.writeHead(200, {
-        "Content-Type": "text/html",
-      });
-      res.write(
-        `<p>${newLink.url} successfully added. Id is ${newLink.id}</p>`
-      );
-      res.write(renderList(data));
-      res.end();
-    }
-  }
-});
-
-app.put("/links/:id", (req, res) => {
-  const id = req.params.id;
-  let link = data.find((link) => link.id === id);
-  if (link) {
-    let body = req.body;
-    console.log("body", body);
-    let newLink = body.url;
-    let prevLink = link.url;
-    link.url = newLink;
-    if (accepts(req) === "json") {
-      res.writeHead(200, {
-        "Content-Type": "application/json",
-      });
-      res.end(
-        JSON.stringify({
-          message: `${prevLink} successfully updated to ${newLink}.`,
-          links: data,
-        })
-      );
-    } else {
-      res.writeHead(200, {
-        "Content-Type": "text/html",
-      });
-      res.write(`<p>${prevLink} successfully updated to ${newLink}.</p>`);
-      res.write(renderList(data));
-      res.end();
-    }
-  } else {
-    res.statusCode = 404;
-    res.end(
-      JSON.stringify({
-        status: 404,
-        message: "Cannot update link",
-      })
-    );
-  }
-});
-
-app.delete("/links/:id", (req, res) => {
-  let id = req.params.id;
-  let link = data.find((link) => link.id === id);
-  if (link) {
-    let linkData = link;
-    data = data.filter((link) => link.id != req.params.id);
-    if (accepts(req) === "json") {
-      res.writeHead(200, {
-        "Content-Type": "application/json",
-      });
-      res.end(
-        JSON.stringify({
-          message: `${linkData.url} with id: ${linkData.id} deleted successfully.`,
-          links: data,
-        })
-      );
-    } else {
-      res.writeHead(200, {
-        "Content-Type": "text/html",
-      });
-      res.write(
-        `<p> ${linkData.url} with id: ${linkData.id} deleted successfully.</p>`
-      );
-      res.write(renderList(data));
-      res.end();
-    }
-  } else {
-    res.statusCode = 404;
-    res.end(
-      JSON.stringify({
-        status: 404,
-        message: "Cannot delete link",
-      })
-    );
-  }
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use("/api", links);
 
 app.listen(port, (err) => {
   if (err) throw err;
@@ -239,19 +38,20 @@ app.listen(port, (err) => {
 });
 
 //exit events
-function save() {
-  //console.log("saved file", data);
+function saveFile() {
+  console.log("data successfully saveFiled");
+
   fs.writeFileSync(dataPath, JSON.stringify(data));
 }
 
 //catches ctrl+c event
 process.on("SIGINT", () => {
-  save();
+  saveFile();
   process.exit();
 });
 // test by using kill [PID_number]
 process.on("SIGTERM", () => {
-  save();
+  saveFile();
   server.close(() => {
     console.log("SIGTERM signal received.");
   });
