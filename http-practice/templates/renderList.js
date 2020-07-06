885e609c2ce2554b1b5449797ce06d50c6ab8415
Rethabile Segoe <precious.segoe@gmail.com>
2020-07-06 16:51:32 +0200

    add styling

--- http-practice/templates/renderList.js
+++ http-practice/templates/renderList.js
@@ -1,12 +1,12 @@
 module.exports = function renderList(data) {
   return `<body>
     <div>
-    <form action="http://localhost:3000/api/links/" method="POST">
+    <form action="http://localhost:3000/api/links/" method="POST" style="display:flex; align-items:center; flex-direction:column; text-align:center;" >
     <div class="form-control">
-      <label for="">New Link</label>
-      <input type="url" " name="url" placeholder="Enter the link here" />
+      <h3>New Link</h3>
+      <input type="url"  name="url" placeholder="Enter the link here" style="padding:0.5em; border-radius:6px;" />
     </div>
-    <input  type="submit" class="btn"></input>
+    <input type="submit" class="btn" style="padding:0.5em; margin-top:1em;"></input>
   </form>
       <h3>Links</h3>
     ${data.map((el) => {
@@ -14,11 +14,11 @@ module.exports = function renderList(data) {
       <div>
       <form action="http://localhost:3000/api/links/put/${el.id}" method="POST">
       <p> <strong>url:</strong> ${el.url}  <strong>id:</strong> ${el.id}</p>
-      <input type="url" " name="url" placeholder="Updated link here" />
-      <button  type="submit" class="btn">Submit</button>
+      <input type="url" " name="url" placeholder="Updated link here" style="padding:0.5em; border-radius:6px; display:block;" />
+      <button type="submit" class="btn" style="padding:0.5em; margin-top:1em;">Submit</button>
       </form>
       <form action="http://localhost:3000/api/links/delete/${el.id}" method="POST">
-      <button type="submit" class="btn" name="url" name="delete">Delete</button>
+      <button type="submit" class="btn" name="url" name="delete" style="padding:0.5em; margin-top:1em;">Delete</button>
       </form>
       </div>
       `;
