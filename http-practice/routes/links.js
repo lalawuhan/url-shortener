f2ada7d2319cbcf2ceebec340a4b78f74cf74729
Rethabile Segoe <precious.segoe@gmail.com>
2020-07-06 16:06:06 +0200

    add delete and update buttons and routes

--- http-practice/routes/links.js
+++ http-practice/routes/links.js
@@ -1,8 +1,5 @@
 const polka = require("polka");
 const router = polka();
-const fs = require("fs");
-const path = require("path");
-const homePage = path.join(__dirname, "../index.html");
 const renderList = require("../templates/renderList");
 const generateID = require("../generateNum");
 let { data } = require("../data/data");
@@ -15,26 +12,21 @@ function accepts(req) {
 }
 
 router.get("/", (req, res) => {
-  fs.readFile(homePage, { encoding: "utf-8" }, function (err, data) {
-    if (!err) {
-      if (accepts(req) === "json") {
-        res.writeHead(200, {
-          "Content-Type": "application/json",
-        });
-        res.end(
-          JSON.stringify({
-            links: data,
-          })
-        );
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(data);
-        res.end();
-      }
-    } else {
-      console.log(err);
-    }
-  });
+  if (accepts(req) === "json") {
+    res.writeHead(200, {
+      "Content-Type": "application/json",
+    });
+    res.end(
+      JSON.stringify({
+        links: data,
+      })
+    );
+  } else {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    res.end(renderList(data));
+  }
 });
 
 router.get("/links", (req, res) => {
@@ -161,6 +153,81 @@ router.put("/links/:id", (req, res) => {
 });
 
 router.delete("/links/:id", (req, res) => {
+  let id = req.params.id;
+  let link = data.find((link) => link.id === id);
+  if (link) {
+    let linkData = link;
+    data = data.filter((link) => link.id != req.params.id);
+    if (accepts(req) === "json") {
+      res.writeHead(200, {
+        "Content-Type": "application/json",
+      });
+      res.end(
+        JSON.stringify({
+          message: `${linkData.url} with id: ${linkData.id} deleted successfully.`,
+          links: data,
+        })
+      );
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+      res.write(
+        `<p> ${linkData.url} with id: ${linkData.id} deleted successfully.</p>`
+      );
+      res.write(renderList(data));
+      res.end();
+    }
+  } else {
+    res.statusCode = 404;
+    res.end(
+      JSON.stringify({
+        status: 404,
+        message: "Cannot delete link",
+      })
+    );
+  }
+});
+
+//put and delete form
+router.post("/links/put/:id", (req, res) => {
+  const id = req.params.id;
+  let link = data.find((link) => link.id === id);
+  if (link) {
+    let body = req.body;
+    let newLink = body.url;
+    let prevLink = link.url;
+    link.url = newLink;
+    if (accepts(req) === "json") {
+      res.writeHead(200, {
+        "Content-Type": "application/json",
+      });
+      res.end(
+        JSON.stringify({
+          message: `${prevLink} successfully updated to ${newLink}.`,
+          links: data,
+        })
+      );
+    } else {
+      res.writeHead(200, {
+        "Content-Type": "text/html",
+      });
+      res.write(`<p>${prevLink} successfully updated to ${newLink}.</p>`);
+      res.write(renderList(data));
+      res.end();
+    }
+  } else {
+    res.statusCode = 404;
+    res.end(
+      JSON.stringify({
+        status: 404,
+        message: "Cannot update link",
+      })
+    );
+  }
+});
+
+router.post("/links/delete/:id", (req, res) => {
   let id = req.params.id;
   let link = data.find((link) => link.id === id);
   if (link) {
--- http-practice/templates/renderList.js
+++ http-practice/templates/renderList.js
@@ -1,13 +1,29 @@
 module.exports = function renderList(data) {
   return `<body>
-    <ul>
+    <div>
+    <form action="http://localhost:3000/api/links/" method="POST">
+    <div class="form-control">
+      <label for="">New Link</label>
+      <input type="text" " name="url" placeholder="Enter the link here" />
+    </div>
+    <input  type="submit" class="btn"></input>
+  </form>
+      <h3>Links</h3>
     ${data.map((el) => {
       return `
-      <li> <strong>url:</strong> ${el.url}</li>
-      <li> <strong>id:</strong> ${el.id}</li>
+      <div>
+      <form action="http://localhost:3000/api/links/put/${el.id}" method="POST">
+      <p> <strong>url:</strong> ${el.url}  <strong>id:</strong> ${el.id}</p>
+      <input type="text" " name="url" placeholder="Updated link here" />
+      <button  type="submit" class="btn">Submit</button>
+      </form>
+      <form action="http://localhost:3000/api/links/delete/${el.id}" method="POST">
+      <button type="submit" class="btn" name="url" name="delete">Delete</button>
+      </form>
+      </div>
       `;
     })}
-    </ul>
+    </div>
     </body>
     `;
 };
